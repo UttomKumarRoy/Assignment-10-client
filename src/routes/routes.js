c053d8d4e5e53de3ce2782925fc8a0a87267f2a9
Uttom <uttom.teacher.gps2@gmail.com>
2022-10-24 23:39:51 +0600

    All routes added

---
+++ src/routes/routes.js
@@ -0,0 +1,44 @@
+import { createBrowserRouter } from 'react-router-dom'
+import ErrorPage from '../components/ErrorPage'
+import Home from '../components/Home'
+import Login from '../components/Login'
+import Register from '../components/Register'
+import Main from '../layout/Main'
+import Profile from '../components/Profile'
+import PrivateRoute from './PrivateRoute'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/home',
+        element: <Home />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/register',
+        element: <Register />,
+      },
+      {
+        path: '/profile',
+        element: (
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+])
+
+export default router
