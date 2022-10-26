c63b5c7ed350b6e6a1317b73d3e92624544e5c71
Uttom <uttom.teacher.gps2@gmail.com>
2022-10-27 04:27:27 +0600

    React PDF button added

--- src/components/CourseDetails.js
+++ src/components/CourseDetails.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { useLoaderData } from 'react-router-dom';
+import ReactToPrint from 'react-to-print';
 
 const CourseDetails = () => {
-    const course=useLoaderData()
+    const ref=useRef();
+    const course=useLoaderData();
    const {name, image, description, price}=course
     return (
         <div>
+            <div className='text-center'>
+            <ReactToPrint  trigger={() => {
+            return <button className='bg-success text-white p-2 rounded-3'>Download PDF</button>;
+          }}
+          content={() => ref.current}
+        />
+            </div>
+            
+        <div ref={ref}>
             <div className='d-flex justify-content-between align-items-center'>
                 <h2>Course Title: {name}</h2>
-                <button>Download PDF</button>
+                
+                
             </div>
             <img height={200} className='img-sm ' src={image} alt="this is pic" />
             <h3>Course Description: {description}</h3>
             <p>Course Price: {price}</p>
+        </div>
+           
             <button>Get Premium Access</button>
         </div>
     );
