f03037423023f2107d56b00197cd40cf04dcbfd0
Uttom <uttom.teacher.gps2@gmail.com>
2022-10-27 09:41:04 +0600

    Responsive done and Readme file added

--- src/components/CourseRight.js
+++ src/components/CourseRight.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const CourseRight = (props) => {
     const {id, name, image, price}=props.course;
     return (
-        <div style={{border:"2px solid green"}} className='col-lg-4 g-3'>
+        <div style={{border:"2px solid green"}} className='col-sm-12 col-md-6 col-lg-4 g-3'>
             <h4>Course Title: {name}</h4>
             <img height={200} className='img-fluid' src={image} alt="This is course" />
             <p>Price: {price}</p>
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -8,13 +8,13 @@ const Courses = () => {
 //   const [id, name, price, image, description]=courses;
     return (
         <div className='row'>
-            <div className='col-lg-3'>
+            <div className='col-sm-12 col-md-3'>
                 <h2 className='bg-primary text-white rounded-2 p-2'>Courses in Menu</h2>
                 {
                 courses.map(course => <CourseLeft key={course.id} course={course} />)
             },
             </div>
-            <div className='col-lg-9'>
+            <div className='col-sm-12 col-md-9'>
                 <div className='row'>
                     <h2 className='bg-primary text-white rounded-2 p-2'>Courses in Cards</h2>
                 {
